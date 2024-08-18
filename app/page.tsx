40235c519fcf00492311ba52325a635f4b99fde9
Timothy Cook <tim.wade.cook@gmail.com>
2024-08-18 14:17:06 -0400

    create simple home page with stock picker nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,48 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+import { stocks } from "@/lib/data"
+import { cn } from "@/lib/utils"
+
+import Link from "next/link"
+
 export default function Home() {
-  return <main>hello world</main>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-4 bg-zinc-50">
+      <section>
+        <NavigationMenu>
+          <NavigationMenuList>
+            <NavigationMenuItem>
+              <NavigationMenuTrigger className="w-72 border">
+                Pick a stock
+              </NavigationMenuTrigger>
+              <NavigationMenuContent className="max-h-72 overflow-scroll">
+                {stocks.map((s) => (
+                  <Link
+                    className=""
+                    key={s.symbol}
+                    href={`#`}
+                    legacyBehavior
+                    passHref
+                  >
+                    <NavigationMenuLink
+                      className={cn(navigationMenuTriggerStyle(), "w-72")}
+                    >
+                      {s.displayName}
+                    </NavigationMenuLink>
+                  </Link>
+                ))}
+              </NavigationMenuContent>
+            </NavigationMenuItem>
+          </NavigationMenuList>
+        </NavigationMenu>
+      </section>
+    </main>
+  )
 }
---
+++ lib/data.ts
@@ -0,0 +1,88 @@
+export const stocks = [
+  {
+    displayName: "CrowdStrike (CRWD)",
+    symbol: "CRWD",
+  },
+  {
+    displayName: "PayPal (PYPL)",
+    symbol: "PYPL",
+  },
+  {
+    displayName: "Airbnb (ABNB)",
+    symbol: "ABNB",
+  },
+  {
+    displayName: "Shopify (SHOP)",
+    symbol: "SHOP",
+  },
+  {
+    displayName: "MercadoLibre (MELI)",
+    symbol: "MELI",
+  },
+  {
+    displayName: "Walt Disney (DIS)",
+    symbol: "DIS",
+  },
+  {
+    displayName: "Intuitive Surgical (ISRG)",
+    symbol: "ISRG",
+  },
+  {
+    displayName: "Berkshire Hathaway (BRK.A)",
+    symbol: "BRK.A",
+  },
+  {
+    displayName: "Amazon (AMZN)",
+    symbol: "AMZN",
+  },
+  {
+    displayName: "Alphabet (GOOGL)",
+    symbol: "GOOGL",
+  },
+
+  {
+    displayName: "Tesla (TSLA)",
+    symbol: "TSLA",
+  },
+
+  {
+    displayName: "AST SpaceMobile (ASTS)",
+    symbol: "ASTS",
+  },
+
+  {
+    displayName: "Rocket Lab USA (RKLB)",
+    symbol: "RKLB",
+  },
+
+  {
+    displayName: "Palantir (PLTR)",
+    symbol: "PLTR",
+  },
+
+  {
+    displayName: "Pfizer (PFE)",
+    symbol: "PFE",
+  },
+
+  {
+    displayName: "Apple (AAPL)",
+    symbol: "AAPL",
+  },
+  {
+    displayName: "Marathon (MARA)",
+    symbol: "MARA",
+  },
+  {
+    displayName: "Banco Bradesco (BBD)",
+    symbol: "BBD",
+  },
+  {
+    displayName: "Nu Holdings (NU)",
+    symbol: "NU",
+  },
+  {
+    displayName: "Advanced Micro Devices (AMD)",
+    symbol: "AMD",
+  },
+];
