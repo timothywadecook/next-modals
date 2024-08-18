6fa2fd444b3c09c976391a651239a55228e72d28
Timothy Cook <tim.wade.cook@gmail.com>
2024-08-18 16:11:03 -0400

    make placeholder chart page encountered on refresh or sharing

--- app/chart/[symbol]/[limit]/page.tsx
+++ app/chart/[symbol]/[limit]/page.tsx
@@ -1,8 +1,44 @@
-// This is to handle link sharing and hard browser refresh from stock chart modal.
+import { BarChart } from "@/components/charts/bar-chart"
+import { BarChartSkeleton } from "@/components/charts/bar-chart-skeleton"
+import { buttonVariants } from "@/components/ui/button"
+import { stocks } from "@/lib/data"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import { notFound } from "next/navigation"
+import { Suspense } from "react"
+
 type Props = {
   params: { symbol: string; limit: string }
 }
 
-export default function ChartPage(props: Props) {
-  return <div>non-modal chart page</div>
+export default async function ChartPage({
+  params: { symbol, limit: limitStr },
+}: Props) {
+  const stock = stocks.find((one) => one.symbol === symbol)
+  const limit = Number(limitStr)
+  if (!stock || isNaN(limit)) {
+    return notFound()
+  }
+
+  return (
+    <div className="w-full min-h-screen flex flex-col  bg-zinc-50">
+      <div className="max-w-6xl w-full  mx-auto flex flex-col px-4 py-10">
+        <h1 className="text-4xl mb-2">{stock.displayName}</h1>
+        <p className="mb-4">
+          Note: This page is just a <em>placeholder</em> to handle link sharing
+          and hard browser refresh from the stock chart modal.
+        </p>
+        <div className="h-96 w-full">
+          <Suspense fallback={<BarChartSkeleton maxBarHeightPx={280} />}>
+            <BarChart symbol={symbol} limit={limit} maxBarHeightPx={280} />
+          </Suspense>
+        </div>
+        <div className="flex flex-col md:flex-row md:justify-end">
+          <Link className={cn(buttonVariants({ variant: "default" }))} href="/">
+            Go Home
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
